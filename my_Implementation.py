47b627fa76ce8e9927ea75f948f8a8d9d41dda07
morteza <65276280+mori-cyber@users.noreply.github.com>
2021-05-30 15:32:57 +0430

    Add files via upload

--- my_Implementation.py
+++ my_Implementation.py
@@ -1,126 +1,140 @@
-import tensorflow as tf
-# import keras
 import matplotlib.pyplot as plt
-from tensorflow.keras.applications import VGG19
-import caer
-# import canaro
-import cv2 as cv
-conv_base = VGG19(weights='imagenet',
-                  include_top=False,
-                  input_shape=(150, 150, 3))
-# tf.keras.utils.plot_model(conv_base, show_shapes=True)
-# --------------------------------------------------------------------------------------------------
-# conv_base.summary()
+import tensorflow
+# from tensorflow import keras
+# import keras
+
+
+
+import tensorflow.keras as keras
 import os
-import numpy as np
-import pandas as pd
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
-base_dir = r'C:\Users\diana\Pictures\Data\dogcat'
-train_dir = os.path.join(base_dir, 'train')
-validation_dir = os.path.join(base_dir, 'validation')
-test_dir = os.path.join(base_dir, 'test')
-datagen = ImageDataGenerator(rescale=1. / 255)
-batch_size = 25
-
-
-# ------------------------------------------------------------------------------------------------------
-
-def extract_features(directory, sample_count):
-    features = np.zeros(shape=(sample_count, 4, 4, 512))
-    labels = np.zeros(shape=(sample_count))
-    generator = datagen.flow_from_directory(
-        directory,
-        target_size=(150, 150),
-        batch_size=batch_size,
-        class_mode='binary')
-    i = 0
-    for inputs_batch, labels_batch in generator:
-        features_batch = conv_base.predict(inputs_batch)
-        features[i * batch_size: (i + 1) * batch_size] = features_batch
-        labels[i * batch_size: (i + 1) * batch_size] = labels_batch
-        i += 1
-        if i * batch_size >= sample_count:
-            # Note that since generators yield data indefinitely in a loop, # we must `b
-            # reak` after every
-            break
-    return features, labels
-
-
-# -----------------------------------------------------------------------------------------------------------
-
-train_features, train_labels = extract_features(train_dir, 200)
-validation_features, validation_labels = extract_features(validation_dir, 100)
-test_features, test_labels = extract_features(test_dir, 100)
-# ----------------------------------------------------------------------------------------------------------
-train_features = np.reshape(train_features, (200, 4 * 4 * 512))
-validation_features = np.reshape(validation_features, (100, 4 * 4 * 512))
-test_features = np.reshape(test_features, (100, 4 * 4 * 512))
-# ----------------------------------------------------------------------------------------------------------
 from tensorflow.keras import models
 from tensorflow.keras import layers
 from tensorflow.keras import optimizers
+import numpy as np
+import pandas as pd
+
+# ----------------------------------------------------------------------------
+
+from tensorflow.keras.applications.resnet50 import ResNet50
+conv_base = ResNet50(weights='imagenet',
+include_top=False,
+input_shape=(32, 32, 3))
+
+# -----------------------------------------------------------------------------
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+# from keras.datasets import cifar10
+# base_dir=cifar10.load_data()
+base_dir =r'C:\Users\diana\Pictures\Data\dogcat'
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+test_dir = os.path.join(base_dir, 'test')
+datagen = ImageDataGenerator(rescale=1./255)
+batch_size = 25
+
+# _______________________________________________________________________________________
+
+def extract_features(directory, sample_count):
+    features = np.zeros(shape=(sample_count, 5,5,2048))
+    labels = np.zeros(shape=(sample_count))
+    generator = datagen.flow_from_directory(
+        directory,
+        target_size=(32, 32),
+        batch_size=batch_size,
+        class_mode='binary'
+        )
+    i = 0
+    for inputs_batch, labels_batch in generator:
+        features_batch = conv_base.predict(inputs_batch)
+        features[i * batch_size : (i + 1) * batch_size] = features_batch
+        labels[i * batch_size : (i + 1) * batch_size] = labels_batch
+        i += 1
+        if i * batch_size >= sample_count:
+        # Note that since generators yield data indefinitely in a loop, # we must `b
+        # reak` after every image has been seen once.
+            break
+    return features, labels
+
+# ---------------------------------------------------------------------------------------
+train_features, train_labels = extract_features(train_dir, 400)
+validation_features, validation_labels = extract_features(validation_dir, 100)
+test_features, test_labels = extract_features(test_dir, 100)
+
+# --------------------------------------------------------------------------------------
+
+train_features = np.reshape(train_features, (400, 5*5*2048))
+train_features = train_features.astype('float32')/255.0-0.5
+validation_features = np.reshape(validation_features, (100, 5*5*2048))
+validation_features = validation_features.astype('float32')/255.0-0.5
+test_features = np.reshape(test_features, (100, 5*5*2048))
+test_features = test_features.astype('float32')/255.0-0.5
+
+# ___________________________________________________________________________________________
+
 
 model = models.Sequential()
-model.add(layers.Dense(256, activation='relu', input_dim=4*4*512))
+model.add(layers.Dense(256, activation='elu', input_dim=5*5*2048))
+model.add(layers.Dense(128, activation='elu'))
+model.add(layers.Dense(64, activation='elu'))
+# model.add(layers.Dense(32, activation='relu'))
 model.add(layers.Dropout(0.5))
 model.add(layers.Dense(1, activation='sigmoid'))
-model.compile(optimizer=optimizers.RMSprop(lr=2e-5),
-              loss='binary_crossentropy',
-              metrics=['acc'])
+model.compile(optimizer='adam',
+loss='binary_crossentropy',
+metrics=['acc'])
 
-# ---------------------------------------------------------------------------------------------------------
+# ______________________________________________________________________________________
 
 history = model.fit(train_features, train_labels,
-                    epochs=10,
-                    batch_size=20,
-                    validation_data=(validation_features, validation_labels))
+epochs=10,
+batch_size=20,
+validation_data=(validation_features, validation_labels))
 
-evalu_test = model.evaluate(test_features, test_labels)
+# ________________________________________________________________________________________
 
-print('this is evaluation of test', evalu_test)
+test_loss, test_acc = model.evaluate(test_features, test_labels, verbose=1)
+print('\nTest accuracy:', test_acc)
+# print(ndim.test_features[20])
 
-# --------------------------------------------------------------------------------------------------------
+# ___________________________________________________________________________________________
+import sklearn
+images = train_features
+codes = model.predict(images)
+assert  len(codes) == len(images)
+from sklearn.neighbors._unsupervised import  NearestNeighbors
+nei_clf = NearestNeighbors(metric='euclidean')
+nei_clf.fit(codes)
+
+# ----------------------------------------------------------------------------------------
+
+def get_similar(image,n_neighbors=5):
+    # try:
+    # image = plt.imread(image)
+    assert image.ndim == 1
+    code = model.predict(image[0])
+    (distances,),(idx,) = nei_clf.kneighbors(code,n_neighbors = n_neighbors)
+    # except AssertionError:
+    #     print('i amangry')
+    #     exit(1)
+    return distances,images[idx]
+# ----------------------------------------------------------------------------------------
+def show_image(x):
+    plt.imshow(np.clip(x+0.5,0,1))
+
+def show_similar(image):
+    distaces ,neighbors = get_similar(image,n_neighbors = 3)
+    plt.figure(figsize=[6,7])
+    plt.subplot(1,4,1)
+    show_image(image)
+    plt.title("Original image")
+    for i in range(3):
+        plt.subplot(1,4,i+2)
+        show_image(neighbors[i])
+        plt.title("Dis=%3f"% distaces[i])
+    plt.show()
+
+# -------------------------------------------------------------
+# print(np.ndim(test_features[50]))
+show_similar(test_features[50])
 
 
-
-# acc = history.history['acc']
-# val_acc = history.history['val_acc']
-# loss = history.history['loss']
-# val_loss = history.history['val_loss']
-# epochs = range(len(acc))
-# plt.plot(epochs, acc, 'bo', label='Training acc')
-# plt.plot(epochs, val_acc, 'b', label='Validation acc')
-#
-# plt.title('Training and validation accuracy')
-# plt.legend()
-# plt.figure()
-# plt.plot(epochs, loss, 'bo', label='Training loss')
-# plt.plot(epochs, val_loss, 'b', label='Validation loss')
-# plt.title('Training and validation loss')
-# plt.legend()
-# plt.show(block=True)
-# plt.interactive(False)
-# -------------------------------------------------------------------------------------------------------
-
-
-
-# test_path =r'C:\Users\diana\Pictures\Data\dogcat\test\test1\20.jpg'
-img = cv.imread(r'C:\Users\diana\Pictures\Data\dogcat\test\test1\20.jpg')
-# plt.imshow('a',img)
-IMG_SIZE=(150,150)
-def prepare(img):
-    img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-
-# f = cv.imread('C:/Users/Music/university_project/Data/images1.jpg')
-    img = cv.resize(img, IMG_SIZE)
-
-    img = img / 255.0
-    img = caer.reshape(img, IMG_SIZE,3)
-    return img
-preduction = model.predict(prepare(img))
-print(np.argmax(preduction))
-# y_hat = model.predict(img)
-# print((y_hat))
-cv.imshow(img)
-plt.show(img)
